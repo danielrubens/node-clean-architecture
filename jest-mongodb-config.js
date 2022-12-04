9c459f76befba5d73ae83d2d7f9579c33f001d7b
danielrubens <danielrubensdaniel@gmail.com>
2022-12-04 15:31:56 -0300

    chore: add jest config for mongodb

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -4,8 +4,8 @@ module.exports = {
       dbName: 'jest'
     },
     binary: {
-    //   version: '4.0.3',
-      version: '4.12.1',
+      version: '4.0.3',
+      //   version: '4.12.1',
       skipMD5: true
     },
     autoStart: true
